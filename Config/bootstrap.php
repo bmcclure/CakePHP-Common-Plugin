dd94f0f1c43835d888bd368e6e9643c0653aab7d
Christian Winther <cw@nodes.dk>
2012-03-08 20:44:33 +0100

    big N

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 App::import('Lib', 'Common.Nodes/Autoload');
+
 Nodes\Autoload::register();
 Nodes\Autoload::addPath(App::path('Lib'));
 Nodes\Autoload::addPath(App::pluginPath('Common') . 'Lib' . DS);
