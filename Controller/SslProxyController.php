02e90372403cf66e80a6db90b4f858dfe00a9a00
Christian Winther <cw@nodes.dk>
2012-04-20 21:40:56 +0200

    Adding SslProxyController
    It allows us to proxy non-https content through https in the backend and platform
    
    Useful for showing non-secure images on a secure page with javascript

---
+++ Controller/SslProxyController.php
@@ -0,0 +1,35 @@
+<?php
+App::uses('Validation', 'Utility');
+
+class SslProxyController extends CommonAppController {
+
+	public $uses = array();
+
+	public function view() {
+		if (empty($this->request->query['link'])) {
+			throw new NotFoundException('Missing link parameter');
+		}
+
+		$link = $this->request->query['link'];
+
+		if (!Validation::url($link)) {
+			throw new NotFoundException('Invalid link');
+		}
+
+
+		$request = new Nodes\Curl($link);
+		$request->exec();
+
+		$responseCode = $request->getResponseCode();
+		if ($responseCode != 200) {
+			throw new NotFoundException('Response code was not 200 OK');
+		}
+
+		$responseHeaders = $request->getResponseHeaders();
+		$this->response->disableCache();
+		$this->response->type($request->getResponseHeader('content-type'));
+		$this->response->body($request->getResponseBody());
+		$this->response->send();
+		die;
+	}
+}
